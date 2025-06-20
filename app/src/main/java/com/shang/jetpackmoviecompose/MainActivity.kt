6b47e195a32f6dd3c365fc9a1edc54b614ad2dc3
CaiShangLin <nick034po@yahoo.com.tw>
2025-06-20 13:14:05 +0800

    feat: 更新主題和依賴以支援新功能
    - 修改 AndroidManifest.xml 以使用新的啟動畫面主題。
    - 更新 build.gradle 中的依賴版本以提升功能和性能。
    - 新增顏色和主題相關的檔案以改善 UI 設計。

--- app/src/main/java/com/shang/jetpackmoviecompose/MainActivity.kt
+++ app/src/main/java/com/shang/jetpackmoviecompose/MainActivity.kt
@@ -2,12 +2,30 @@ package com.shang.jetpackmoviecompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.shang.jetpackmoviecompose.theme.JetpackMovieComposeTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        setContent{
+            JetpackMovieComposeTheme{
+                Scaffold {_->
+
+                    Text("Hello Jetpack Compose!")
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/shang/jetpackmoviecompose/theme/Theme.kt
@@ -0,0 +1,88 @@
+package com.shang.jetpackmoviecompose.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80,
+    background = Color.Black
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40,
+    background = Color.White,
+)
+
+data class MyColor(
+    val bottomTabBackground: Color,
+    val cardBackground: Color,
+    val homeTabBackground: Color,
+    val textColor: Color
+)
+
+private val Light = MyColor(
+    bottomTabBackground = Color.White,
+    cardBackground = Color.White,
+    homeTabBackground = Color.White,
+    textColor = Color.Black
+)
+
+private val Dark = MyColor(
+    bottomTabBackground = Black_272727,
+    cardBackground = Black_272727,
+    homeTabBackground = Black_272727,
+    textColor = Color.White
+)
+
+var MyColorScheme = Light
+
+@Composable
+fun JetpackMovieComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+    MyColorScheme = if (darkTheme) {
+        Dark
+    } else {
+        Light
+    }
+//    val colorScheme = when {
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
+//        darkTheme -> DarkColorScheme
+//        else -> LightColorScheme
+//    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
