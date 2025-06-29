ee2c049d82b257822006522afbb4eed6574eb3e3
CaiShangLin <nick034po@yahoo.com.tw>
2025-06-29 13:11:02 +0800

    feat: 新增電影資料來源實作及網絡模組配置

--- core/network/src/main/java/com/shang/network/retrofit/MovieDataSourceImp.kt
+++ core/network/src/main/java/com/shang/network/retrofit/MovieDataSourceImp.kt
@@ -1,10 +1,16 @@
 package com.shang.network.retrofit
 
-// class MovieDataSourceImp @Inject constructor(private val _movieApiService: MovieApiService) :
-//    MovieDataSource {
-//    override suspend fun getMovieGenres(): MovieGenreBean {
-//        val json = _movieApiService.getMovieGenres().body()?.string() ?: ""
-//        val bean: MovieGenreResponse = Json.Default.decodeFromString<MovieGenreResponse>(json)
-//        return bean.asExternalModel()
-//    }
-// }
+import com.shang.model.MovieGenreBean
+import com.shang.network.model.MovieGenreResponse
+import com.shang.network.model.asExternalModel
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
+
+class MovieDataSourceImp @Inject constructor(private val _movieApiService: MovieApiService) :
+    MovieDataSource {
+    override suspend fun getMovieGenres(): MovieGenreBean {
+        val json = _movieApiService.getMovieGenres().body()?.string() ?: ""
+        val bean: MovieGenreResponse = Json.Default.decodeFromString<MovieGenreResponse>(json)
+        return bean.asExternalModel()
+    }
+ }
