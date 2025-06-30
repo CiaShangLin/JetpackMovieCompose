0d8c0cf080c88420b41c74500cd941425e308b42
shang_cai@appdet.com <as6911809>
2025-06-30 16:20:35 +0800

    refactor: 移除不必要的 Kotlin Gradle 插件依賴

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,6 @@ repositories{
 dependencies{
     api(kotlin("gradle-plugin:2.0.21"))
     implementation("com.android.tools.build:gradle:8.9.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
     implementation("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
     implementation("com.squareup:javapoet:1.13.0")
