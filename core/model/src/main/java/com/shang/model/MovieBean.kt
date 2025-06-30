76ac1a6805294ae7581df251fe7b2fcafe5c9e62
shang_cai@appdet.com <as6911809>
2025-06-30 14:33:31 +0800

    feat: 新增 MovieBean 資料類別

---
+++ core/model/src/main/java/com/shang/model/MovieBean.kt
@@ -0,0 +1,10 @@
+package com.shang.model
+
+data class MovieBean(
+    val id: Int = 0,
+    val title: String = "",
+    val posterPath: String = "",
+    val voteAverage: Double = 0.0,
+    val releaseDate: String = "",
+    val timestamp: Int = 0,
+)
