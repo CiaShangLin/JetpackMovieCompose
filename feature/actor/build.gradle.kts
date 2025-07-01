d9665261b05a066dd998bafbaeb5e2896b416f9d
shang_cai@appdet.com <as6911809>
2025-07-01 15:36:02 +0800

    feat: 新增基本的單元測試和儀表板測試範例

---
+++ feature/actor/build.gradle.kts
@@ -0,0 +1,21 @@
+import deps.androidx
+import deps.testDeps
+import deps.testImplDeps
+import plugs.SharedLibraryGradlePlugin
+
+plugins {
+    id(plugs.BuildPlugins.ANDROID_LIBRARY)
+    id(plugs.BuildPlugins.KOTLIN_COMPOSE) version deps.DependenciesVersions.KOTLIN_COMPILER
+}
+
+apply<SharedLibraryGradlePlugin>()
+
+android {
+    namespace = "com.shang.actor"
+}
+
+dependencies {
+    androidx()
+    testDeps()
+    testImplDeps()
+}
