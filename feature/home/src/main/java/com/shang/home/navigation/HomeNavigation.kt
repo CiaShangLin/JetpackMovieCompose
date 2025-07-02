dab0dd8da4fa54307ba7d70b0fda8062862fd9c6
shang_cai@appdet.com <as6911809>
2025-07-02 11:40:02 +0800

    feat: 更新導航功能以支援 HomeScreen 顯示

--- feature/home/src/main/java/com/shang/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/java/com/shang/home/navigation/HomeNavigation.kt
@@ -1,18 +1,19 @@
 package com.shang.home.navigation
 
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.shang.home.HomeScreen
 import kotlinx.serialization.Serializable
 
 @Serializable
 data object HomeRoute
 
-fun NavController.navigateToHome(navOptions: NavOptions) = navigate(route = HomeRoute.toString(), navOptions)
+fun NavController.navigateToHome(navOptions: NavOptions) = navigate(route = HomeRoute, navOptions)
 
-// fun NavGraphBuilder.composableHomeScreen(
-//    topicDestination: NavGraphBuilder.() -> Unit,
-// ) {
-//    composable(route = HomeRoute.toString()){
-//        HomeScreen()
-//    }
-// }
+ fun NavGraphBuilder.composableHomeScreen() {
+    composable<HomeRoute>(){
+        HomeScreen()
+    }
+ }
