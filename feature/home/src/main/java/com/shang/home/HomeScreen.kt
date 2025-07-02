21b39b7ecb5c2792b39b9764f0d49941a6dfa359
shang_cai@appdet.com <as6911809>
2025-07-02 15:53:50 +0800

    feat: 新增使用者資料顯示功能於 HomeScreen

--- feature/home/src/main/java/com/shang/home/HomeScreen.kt
+++ feature/home/src/main/java/com/shang/home/HomeScreen.kt
@@ -1,22 +1,39 @@
 package com.shang.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.text.TextStyle
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.shang.common.UiState
+import com.shang.model.ConfigurationBean
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
     val configurationState = viewModel.configuration.collectAsStateWithLifecycle()
+    val userData = viewModel.userData.collectAsStateWithLifecycle()
 
-    Text("Home Scree", style = TextStyle(color = MaterialTheme.colorScheme.error))
+    Column {
+        Text("Home Scree", style = TextStyle(color = MaterialTheme.colorScheme.error))
 
-    when (val state = configurationState.value) {
-        is UiState.Loading -> Text("Loading...", style = TextStyle(color = MaterialTheme.colorScheme.primary))
-        is UiState.Success -> Text("Home Screen : ${state.data}", style = TextStyle(color = MaterialTheme.colorScheme.onSurface))
-        is UiState.Error -> Text("Error: ${state.error}", style = TextStyle(color = MaterialTheme.colorScheme.error))
+        when (val state = configurationState.value) {
+            is UiState.Loading -> Text("Loading...", style = TextStyle(color = MaterialTheme.colorScheme.primary))
+            is UiState.Success -> Text("Home Screen : ${state.data}", style = TextStyle(color = MaterialTheme.colorScheme.onSurface))
+            is UiState.Error -> Text("Error: ${state.error}", style = TextStyle(color = MaterialTheme.colorScheme.error))
+        }
+
+        Text("----------", style = TextStyle(color = MaterialTheme.colorScheme.error))
+
+        Text("userData:$userData", style = TextStyle(color = MaterialTheme.colorScheme.error))
+
+        Button(onClick = {
+            viewModel.setConfiguration((configurationState.value as UiState.Success<ConfigurationBean>).data)
+        }) {
+            Text("Click Me", style = TextStyle(color = MaterialTheme.colorScheme.onPrimary))
+        }
     }
 }
--- feature/home/src/main/java/com/shang/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/shang/home/HomeViewModel.kt
@@ -3,17 +3,21 @@ package com.shang.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.shang.common.UiState
+import com.shang.data.repository.UserDataRepository
 import com.shang.domain.usecase.GetConfigurationUseCase
 import com.shang.model.ConfigurationBean
+import com.shang.model.UserData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     getConfigurationUseCase: GetConfigurationUseCase,
+    private val userDataRepository: UserDataRepository,
 ) : ViewModel() {
     // {"id":28,"name":"Action"}
 
@@ -23,4 +27,16 @@ class HomeViewModel @Inject constructor(
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = UiState.Loading,
         )
+
+    val userData = userDataRepository.userData.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = UserData(),
+    )
+
+    fun setConfiguration(configuration: ConfigurationBean) {
+        viewModelScope.launch {
+            userDataRepository.setConfiguration(configuration)
+        }
+    }
 }
