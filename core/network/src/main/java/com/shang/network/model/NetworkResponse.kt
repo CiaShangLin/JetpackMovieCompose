7514624f92eae81f11e8a6d9df83fce76222c915
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-03 21:15:58 +0800

    feat(home): 新增電影類別狀態管理功能

--- core/network/src/main/java/com/shang/network/model/NetworkResponse.kt
+++ core/network/src/main/java/com/shang/network/model/NetworkResponse.kt
@@ -25,3 +25,13 @@ data class NetworkResponse<out T>(
     val isSuccess: Boolean
         get() = code in 200..299 && data != null
 }
+
+// fun <T> NetworkResponse<T>.asFlow(): Flow<Result<T>> {
+//    return flow {
+//        if (isSuccess) {
+//            emit(Result.success(data!!))
+//        } else {
+//            emit(Result.failure(error!!))
+//        }
+//    }
+// }
