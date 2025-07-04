b0961ce6db4fb557bf4df7d00437f46757523179
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-04 22:37:49 +0800

    refactor(ui): 新增 UiModule 以提供圖片加載功能

--- app/src/main/java/com/shang/jetpackmoviecompose/JetpackMovieApplication.kt
+++ app/src/main/java/com/shang/jetpackmoviecompose/JetpackMovieApplication.kt
@@ -1,12 +1,23 @@
 package com.shang.jetpackmoviecompose
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class JetpackMovieApplication : Application() {
+class JetpackMovieApplication : Application(), SingletonImageLoader.Factory {
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
 
     override fun onCreate() {
         super.onCreate()
+    }
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return imageLoader
     }
 }
