bc50cdab0a75c5554a4d1bcefde5b6f6379f2fe8
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-04 15:15:59 +0800

    feat(home): 重構 HomeContentViewModel 以支援電影類別的載入與狀態管理

--- feature/home/src/main/java/com/shang/home/ui/HomeContentViewModel.kt
+++ feature/home/src/main/java/com/shang/home/ui/HomeContentViewModel.kt
@@ -1,31 +1,34 @@
 package com.shang.home.ui
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
 import com.shang.data.repository.MovieRepository
 import com.shang.model.MovieGenreBean
+import com.shang.model.MovieListBean
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 
-@HiltViewModel
-class HomeContentViewModel @Inject constructor(
+@HiltViewModel(assistedFactory = HomeContentViewModel.Factory::class)
+class HomeContentViewModel @AssistedInject constructor(
     private val movieRepository: MovieRepository,
-    private val savedStateHandle: SavedStateHandle,
+    @Assisted val movieGenre: MovieGenreBean.MovieGenre,
 ) : ViewModel() {
 
-    companion object {
-        private const val KEY_MOVIES = "KEY_MOVIES"
+    @AssistedFactory
+    interface Factory {
+        fun create(movieGenre: MovieGenreBean.MovieGenre): HomeContentViewModel
     }
 
-    val movieGenres = savedStateHandle.getStateFlow<Int>(
-        KEY_MOVIES,
-        0,
-    )
-
-    fun setMovieGenre(movieGenre: MovieGenreBean.MovieGenre) {
-        savedStateHandle[KEY_MOVIES] = movieGenre.id
-    }
-
-    init {
-    }
+    val movieList: Flow<PagingData<MovieListBean.Result>> = movieRepository.getMovieGenrePager(movieGenre.id.toString())
+        .stateIn(
+            scope = CoroutineScope(kotlinx.coroutines.Dispatchers.Default),
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = PagingData.empty(),
+        )
 }
