a903885897bf1c8e2a2216b115536d9a86a347b0
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-06 18:05:52 +0800

    feat: 修改 MovieBean 轉換為 MovieCollectEntity

--- core/data/src/main/java/com/shang/data/model/Movie.kt
+++ core/data/src/main/java/com/shang/data/model/Movie.kt
@@ -1,10 +1,10 @@
 package com.shang.data.model
 
-import com.shang.database.entity.MovieEntity
+import com.shang.database.entity.MovieCollectEntity
 import com.shang.model.MovieBean
 
-fun MovieBean.asEntity(): MovieEntity {
-    return MovieEntity(
+fun MovieBean.asEntity(): MovieCollectEntity {
+    return MovieCollectEntity(
         id = id,
         title = title,
         posterPath = posterPath,
