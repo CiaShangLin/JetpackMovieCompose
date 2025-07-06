f3fd991c3781bf0e31e5ed4a0152de24143f5335
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-06 14:41:36 +0800

    feat : 新增JMAsyncImage

---
+++ core/designsystem/src/main/kotlin/com/shang/designsystem/component/JMAsyncImage.kt
@@ -0,0 +1,44 @@
+package com.shang.designsystem.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter.Companion.DefaultTransform
+import coil3.compose.AsyncImagePainter.State
+import coil3.request.ImageRequest
+
+@Composable
+fun JMAsyncImage(
+    model: Any?,
+    contentDescription: String? = null,
+    modifier: Modifier = Modifier,
+    transform: (State) -> State = DefaultTransform,
+    onState: ((State) -> Unit)? = null,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.FillBounds,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+    filterQuality: FilterQuality = DefaultFilterQuality,
+    clipToBounds: Boolean = true,
+) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current).data(model).build(),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        transform = transform,
+        onState = onState,
+        alignment = alignment,
+        contentScale = contentScale,
+        alpha = alpha,
+        colorFilter = colorFilter,
+        filterQuality = filterQuality,
+        clipToBounds = clipToBounds,
+    )
+}
