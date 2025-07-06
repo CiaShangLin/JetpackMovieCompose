32072996c48cfdceba2906ae293d9628fcd44049
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-07 01:11:26 +0800

    build: 更新 Gradle 構建檔以支援新模組

--- feature/collect/build.gradle.kts
+++ feature/collect/build.gradle.kts
@@ -1,11 +1,13 @@
 import deps.androidx
 import deps.dataModule
+import deps.designsystemModule
 import deps.domainModule
 import deps.hilt
 import deps.kotlinx
 import deps.modelModule
 import deps.testDeps
 import deps.testImplDeps
+import deps.uiModule
 import plugs.SharedLibraryGradlePlugin
 
 plugins {
@@ -23,6 +25,9 @@ dependencies {
     androidx()
     hilt()
     kotlinx()
+
+    designsystemModule()
+    uiModule()
     dataModule()
     domainModule()
     modelModule()
