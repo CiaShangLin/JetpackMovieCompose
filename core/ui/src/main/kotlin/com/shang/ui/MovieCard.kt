a49b419af0291d28e66060fff2fd140e8229b763
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-06 14:41:49 +0800

    feat : MovieCard UI完成

--- core/ui/src/main/kotlin/com/shang/ui/MovieCard.kt
+++ core/ui/src/main/kotlin/com/shang/ui/MovieCard.kt
@@ -2,21 +2,34 @@ package com.shang.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
+import com.shang.designsystem.component.JMAsyncImage
 import com.shang.model.MovieListBean
 
-const val DEMO_URL = "https://fastly.picsum.photos/id/1020/400/300.jpg?hmac=tyq3V0QObhO4gvke1hMd7uZOQ2Sd5LwaQYB9zLBdi2w"
+const val DEMO_URL =
+    "https://fastly.picsum.photos/id/1020/400/300.jpg?hmac=tyq3V0QObhO4gvke1hMd7uZOQ2Sd5LwaQYB9zLBdi2w"
 
 @Composable
 fun MovieCard(
@@ -26,53 +39,129 @@ fun MovieCard(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .clip(MaterialTheme.shapes.medium) // 先裁切，這樣背景和邊框都會被裁切
+            .clip(MaterialTheme.shapes.medium)
             .background(
-                color = MaterialTheme.colorScheme.secondaryContainer,
+                color = MaterialTheme.colorScheme.surface,
             )
             .border(
                 width = 1.dp,
-                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                color = MaterialTheme.colorScheme.onSurface,
                 shape = MaterialTheme.shapes.medium,
             ),
     ) {
-        MovieCover(model = data.posterPath)
+        Column() {
+            Box {
+                MovieCover(model = data.posterPath)
+                MovieRating(
+                    modifier = Modifier
+                        .padding(bottom = 8.dp, start = 8.dp)
+                        .align(Alignment.BottomStart),
+                )
+                MovieCollectButton(
+                    modifier = Modifier
+                        .padding(top = 8.dp, end = 8.dp)
+                        .align(Alignment.TopEnd),
+                )
+            }
+            MovieTitle(data.title)
+            MovieReleaseTitle(data.releaseDate)
+        }
     }
 }
 
 @Composable
 fun MovieCover(model: Any) {
-    AsyncImage(
+    JMAsyncImage(
         model = model, // 使用傳入的圖片路徑
         contentDescription = null,
         modifier = Modifier
             .fillMaxSize() // 圖片拉滿父容器
-            .aspectRatio(3f / 4f),
+            .aspectRatio(3f / 4f)
+            .padding(start = 1.dp, end = 1.dp, top = 1.dp),
         contentScale = ContentScale.FillBounds,
     )
 }
 
 @Composable
-fun MovieTitle() {
+fun MovieTitle(title: String) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.titleMedium,
+        color = MaterialTheme.colorScheme.onSurface,
+        modifier = Modifier.padding(start = 8.dp, top = 8.dp, end = 8.dp),
+        maxLines = 2,
+    )
 }
 
 @Composable
-fun MovieReleaseTitle() {
+fun MovieReleaseTitle(releaseDate: String) {
+    Text(
+        text = releaseDate,
+        style = MaterialTheme.typography.titleMedium,
+        color = MaterialTheme.colorScheme.onSurface,
+        modifier = Modifier.padding(8.dp),
+        maxLines = 1,
+    )
 }
 
 @Composable
-fun MovieRating() {
+fun MovieRating(modifier: Modifier) {
+    Row(
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.onSurface, shape = MaterialTheme.shapes.medium)
+            .padding(horizontal = 8.dp, vertical = 2.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Star,
+            contentDescription = "Rating",
+            modifier = Modifier
+                .size(16.dp),
+            tint = MaterialTheme.colorScheme.onPrimary,
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(
+            "8.7",
+            modifier = Modifier.padding(end = 4.dp),
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onPrimary,
+        )
+    }
 }
 
 @Composable
-fun MovieCollectButton() {
+fun MovieCollectButton(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .size(32.dp)
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = MaterialTheme.shapes.small,
+            )
+            .clickable {
+                // 點擊事件
+            },
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            modifier = Modifier.size(20.dp),
+            imageVector = Icons.Rounded.Favorite,
+            contentDescription = "Collect",
+            tint = MaterialTheme.colorScheme.onPrimaryContainer,
+        )
+    }
 }
 
-// @Preview()
-// @Composable
-// fun MovieCardPreview() {
-//    MovieCard(
-//        modifier = Modifier,
-//
-//    )
-// }
+@Preview()
+@Composable
+fun MovieCardPreview() {
+    MovieCard(
+        modifier = Modifier,
+        data = MovieListBean.Result(
+            id = 1,
+            title = "Sample Movie",
+            releaseDate = "2023-10-01",
+            posterPath = DEMO_URL,
+        ),
+    )
+}
