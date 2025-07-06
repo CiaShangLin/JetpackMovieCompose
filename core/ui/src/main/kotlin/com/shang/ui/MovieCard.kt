e1c1df2f15e0a7ce3fed26ead0f364a3cb67b0da
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-06 21:03:12 +0800

    feat(ui): 新增電影收藏按鈕功能，支持收藏狀態切換

--- core/ui/src/main/kotlin/com/shang/ui/MovieCard.kt
+++ core/ui/src/main/kotlin/com/shang/ui/MovieCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -23,12 +24,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.request.ImageRequest
-import coil3.request.error
-import coil3.request.placeholder
 import com.shang.designsystem.component.JMAsyncImage
 import com.shang.model.MovieListBean
 
@@ -67,6 +64,10 @@ fun MovieCard(
                     modifier = Modifier
                         .padding(top = 8.dp, end = 8.dp)
                         .align(Alignment.TopEnd),
+                    isCollect = data.isCollect,
+                    onClick = {
+                        collectClick(data)
+                    },
                 )
             }
             MovieTitle(data.title)
@@ -78,11 +79,12 @@ fun MovieCard(
 @Composable
 fun MovieCover(model: Any) {
     JMAsyncImage(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(model)
-            .placeholder(R.drawable.icon_movie_card_loading)
-            .error(R.drawable.icon_movie_card_error)
-            .build(),
+        model = model,
+//        model = ImageRequest.Builder(LocalContext.current)
+//            .data(model)
+//            .placeholder(R.drawable.icon_movie_card_loading)
+//            .error(R.drawable.icon_movie_card_error)
+//            .build(),
         contentDescription = null,
         modifier = Modifier
             .fillMaxSize() // 圖片拉滿父容器
@@ -140,7 +142,7 @@ fun MovieRating(modifier: Modifier) {
 }
 
 @Composable
-fun MovieCollectButton(modifier: Modifier) {
+fun MovieCollectButton(modifier: Modifier, isCollect: Boolean, onClick: () -> Unit) {
     Box(
         modifier = modifier
             .size(32.dp)
@@ -148,14 +150,16 @@ fun MovieCollectButton(modifier: Modifier) {
                 color = MaterialTheme.colorScheme.primaryContainer,
                 shape = MaterialTheme.shapes.small,
             )
-            .clickable {
-                // 點擊事件
-            },
+            .clickable(onClick = onClick),
         contentAlignment = Alignment.Center,
     ) {
         Icon(
             modifier = Modifier.size(20.dp),
-            imageVector = Icons.Rounded.Favorite,
+            imageVector = if (isCollect) {
+                Icons.Rounded.Favorite
+            } else {
+                Icons.Rounded.FavoriteBorder
+            },
             contentDescription = "Collect",
             tint = MaterialTheme.colorScheme.onPrimaryContainer,
         )
