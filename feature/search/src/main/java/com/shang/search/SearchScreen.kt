e9c04ab397264daa9ccfbcbc27cf2143b2a66ccd
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-07 15:05:41 +0800

    feat(ui): 新增搜尋畫面與搜尋功能
    新增搜尋畫面，包含搜尋欄位與提示文字，並整合 ViewModel 以支援搜尋邏輯。

--- feature/search/src/main/java/com/shang/search/SearchScreen.kt
+++ feature/search/src/main/java/com/shang/search/SearchScreen.kt
@@ -1,11 +1,104 @@
 package com.shang.search
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun SearchScreen() {
-    Text("Search Screen", style = TextStyle(color = MaterialTheme.colorScheme.error))
+fun SearchScreen(viewModel: SearchViewModel = hiltViewModel()) {
+    var inputText by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = inputText,
+            onValueChange = {
+                inputText = it
+            },
+            label = {
+                Text(stringResource(id = R.string.search_movie_hint))
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = stringResource(id = R.string.search_movie_hint),
+                )
+            },
+            textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurface),
+            shape = MaterialTheme.shapes.large,
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                // 設定容器背景顏色
+                focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+
+                // 移除底部指示線
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+            ),
+        )
+        Divider(
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+            color = MaterialTheme.colorScheme.onSurface,
+            thickness = 1.dp,
+        )
+
+        NotSearchScreen()
+    }
+}
+
+@Composable
+fun NotSearchScreen() {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Icon(
+            imageVector = Icons.Rounded.Search,
+            contentDescription = stringResource(id = R.string.search_movie_hint),
+            modifier = Modifier.padding(bottom = 16.dp),
+        )
+        Text(
+            text = stringResource(id = R.string.search_movie_start),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 8.dp),
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+        Text(
+            text = stringResource(id = R.string.search_movie_input_hint),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier,
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SearchScreenPreview() {
+    SearchScreen()
 }
