59fd93ce086e6e3bee0a4aaaf9a4fe48df2c84da
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-07 13:23:38 +0800

    refactor(ui): 更新收藏畫面以支援電影收藏狀態切換功能

--- feature/collect/src/main/kotlin/com/shang/collect/CollectScreen.kt
+++ feature/collect/src/main/kotlin/com/shang/collect/CollectScreen.kt
@@ -16,12 +16,20 @@ import com.shang.ui.MovieCard
 @Composable
 fun CollectScreen(viewModel: CollectViewModel = hiltViewModel()) {
     val allMovieCollect by viewModel.allMovieCollect.collectAsStateWithLifecycle()
-    Text("Collect Screen : $allMovieCollect", style = TextStyle(color = MaterialTheme.colorScheme.error))
+    Text(
+        "Collect Screen : $allMovieCollect",
+        style = TextStyle(color = MaterialTheme.colorScheme.error)
+    )
 
     when (val result = allMovieCollect) {
         is CollectUiState.Error -> CollectErrorScreen(message = "")
         CollectUiState.Loading -> CollectLoadingScreen()
-        is CollectUiState.Success -> CollectSuccessScreen(result.movieCollectList)
+        is CollectUiState.Success -> CollectSuccessScreen(
+            result.movieCollectList,
+            onCollectClick = { movie ->
+                viewModel.toggleMovieCollectStatus(movie)
+            }
+        )
     }
 }
 
@@ -36,14 +44,17 @@ fun CollectErrorScreen(message: String) {
 }
 
 @Composable
-fun CollectSuccessScreen(movieCollectList: List<MovieListBean.Result>) {
+fun CollectSuccessScreen(
+    movieCollectList: List<MovieListBean.Result>,
+    onCollectClick: (MovieListBean.Result) -> Unit
+) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         content = {
             items(movieCollectList) { movie ->
                 MovieCard(
                     data = movie,
-
+                    onCollectClick = onCollectClick,
                 )
             }
         },
--- feature/collect/src/main/kotlin/com/shang/collect/CollectViewModel.kt
+++ feature/collect/src/main/kotlin/com/shang/collect/CollectViewModel.kt
@@ -3,10 +3,13 @@ package com.shang.collect
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.shang.data.repository.MovieRepository
+import com.shang.model.MovieListBean
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,4 +26,14 @@ class CollectViewModel @Inject constructor(
             started = SharingStarted.WhileSubscribed(5_000),
             initialValue = CollectUiState.Loading,
         )
+
+    fun toggleMovieCollectStatus(movieResult: MovieListBean.Result) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (movieResult.isCollect) {
+                movieRepository.deleteMovie(movieResult)
+            } else {
+                movieRepository.insertMovie(movieResult)
+            }
+        }
+    }
 }
