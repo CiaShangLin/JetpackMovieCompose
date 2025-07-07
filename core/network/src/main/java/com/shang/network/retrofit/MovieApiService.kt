98926cce845edbc958e6a8bd7d98f29745b8d1bb
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-07 15:39:04 +0800

    feat(api): 新增電影搜尋 API

--- core/network/src/main/java/com/shang/network/retrofit/MovieApiService.kt
+++ core/network/src/main/java/com/shang/network/retrofit/MovieApiService.kt
@@ -3,6 +3,7 @@ package com.shang.network.retrofit
 import com.shang.network.model.ConfigurationResponse
 import com.shang.network.model.DiscoverMovieResponse
 import com.shang.network.model.MovieGenreResponse
+import com.shang.network.model.SearchMovieResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -21,11 +22,28 @@ interface MovieApiService {
     @GET("genre/movie/list")
     suspend fun getMovieGenres(): Response<MovieGenreResponse>
 
+    /**
+     * 獲取電影列表
+     * @param withGenres 電影類型
+     * @param page 分頁
+     * @return 電影列表
+     */
     @GET("discover/movie")
     suspend fun getDiscoverMovie(
         @Query("with_genres") withGenres: String,
         @Query("page") page: Int,
     ): Response<DiscoverMovieResponse>
+
+    /**
+     * 搜尋電影
+     * @param query 搜尋關鍵字
+     * @param page 分頁
+     */
+    @GET("search/movie")
+    suspend fun getMovieSearch(
+        @Query("query") query: String,
+        @Query("page") page: Int,
+    ): Response<SearchMovieResponse>
 
 //    @GET("movie/{id}}")
 //    suspend fun getMovieDetail(
