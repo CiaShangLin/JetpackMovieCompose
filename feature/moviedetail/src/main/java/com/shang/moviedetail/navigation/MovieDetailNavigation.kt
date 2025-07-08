41fe8c22230f099df2fb086502afdfd3c10b454b
shang_cai@appdet.com <as6911809>
2025-07-08 12:33:24 +0800

    feat(navigation): 新增電影詳情頁面路由參數處理

--- feature/moviedetail/src/main/java/com/shang/moviedetail/navigation/MovieDetailNavigation.kt
+++ feature/moviedetail/src/main/java/com/shang/moviedetail/navigation/MovieDetailNavigation.kt
@@ -1,9 +1,12 @@
 package com.shang.moviedetail.navigation
 
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.shang.moviedetail.MovieDetailScreen
 
 const val ARG_MOVIE_ID = "movieId"
@@ -14,7 +17,15 @@ fun NavController.navigateToMovieDetail(movieId: Int, navOptions: NavOptions? = 
 }
 
 fun NavGraphBuilder.movieDetailScreen() {
-    composable("$MOVIE_DETAIL_ROUTE/{$ARG_MOVIE_ID}") { backStackEntry ->
+    composable(
+        route = "$MOVIE_DETAIL_ROUTE/{$ARG_MOVIE_ID}",
+        arguments = listOf(
+            navArgument(ARG_MOVIE_ID) {
+                type = NavType.IntType
+                defaultValue = 0
+            },
+        ),
+    ) { backStackEntry ->
         val movieId = backStackEntry.arguments?.getInt(ARG_MOVIE_ID) ?: 0
         MovieDetailScreen(movieId = movieId)
     }
