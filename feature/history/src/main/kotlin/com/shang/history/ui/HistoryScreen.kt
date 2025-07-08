d01e6ca1b5041cc424e8753580912ac8ff6ce025
shang_cai@appdet.com <as6911809>
2025-07-08 18:22:03 +0800

    feat(ui): 新增觀看歷史功能與清空歷史選項
    新增觀看歷史畫面，顯示用戶的電影觀看歷史，並提供清空歷史的功能選項。

--- feature/history/src/main/kotlin/com/shang/history/ui/HistoryScreen.kt
+++ feature/history/src/main/kotlin/com/shang/history/ui/HistoryScreen.kt
@@ -1,12 +1,129 @@
 package com.shang.history.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.TabRowDefaults.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.shang.designsystem.component.JMLazyVerticalGrid
+import com.shang.history.R
+import com.shang.model.MovieCardResult
+import com.shang.ui.MovieCard
+import com.shang.ui.MovieCardData
+import com.shang.ui.asMovieCardData
 
 @Composable
 fun HistoryScreen(viewModel: HistoryViewModel = hiltViewModel()) {
-    Text("History Screen", style = TextStyle(color = MaterialTheme.colorScheme.error))
+    val allMovieHistory = viewModel.allMovieHistory.collectAsStateWithLifecycle()
+
+    if (allMovieHistory.value.isEmpty()) {
+        NotDataScreen()
+    } else {
+        HistoryMovieScreen(data = allMovieHistory.value, onCollectClick = {
+            viewModel.toggleMovieCollectStatus(it)
+        })
+    }
+}
+
+@Composable
+private fun NotDataScreen() {
+    Text("No Data")
+}
+
+@Composable
+private fun HistoryMovieScreen(
+    data: List<MovieCardResult>,
+    onCollectClick: (MovieCardData) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        HistoryTitle()
+        Divider(
+            color = MaterialTheme.colorScheme.onSurface,
+            thickness = 1.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+        )
+        JMLazyVerticalGrid(
+            modifier = Modifier
+                .fillMaxSize(), // 外部間距
+            contentPadding = PaddingValues(8.dp), // 外部間距
+            verticalArrangement = Arrangement.spacedBy(8.dp), // 垂直間距
+            horizontalArrangement = Arrangement.spacedBy(8.dp), // 水平間距
+        ) {
+            items(data) { movie ->
+                MovieCard(
+                    data = movie.asMovieCardData(),
+                    onCollectClick = {
+                        onCollectClick.invoke(it)
+                    },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun HistoryTitle() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(id = R.string.history_title),
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier,
+        )
+        Row(
+            modifier = Modifier.clickable {
+            },
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                modifier = Modifier.size(24.dp),
+                imageVector = Icons.Rounded.Delete,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onPrimaryContainer,
+            )
+            Spacer(Modifier.size(4.dp))
+            Text(
+                text = stringResource(id = R.string.history_clear),
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Black,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HistoryMovieScreenPreview() {
+    HistoryMovieScreen(listOf(), onCollectClick = {})
 }
--- feature/history/src/main/kotlin/com/shang/history/ui/HistoryViewModel.kt
+++ feature/history/src/main/kotlin/com/shang/history/ui/HistoryViewModel.kt
@@ -1,13 +1,35 @@
 package com.shang.history.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.shang.data.repository.MovieRepository
+import com.shang.ui.MovieCardData
+import com.shang.ui.asMovieCardResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HistoryViewModel @Inject constructor(private val movieRepository: MovieRepository):
+class HistoryViewModel @Inject constructor(private val movieRepository: MovieRepository) :
     ViewModel() {
 
+    val allMovieHistory = movieRepository.getAllMovieHistory()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = emptyList(),
+        )
 
+    fun toggleMovieCollectStatus(data: MovieCardData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (data.movieCardIsCollect) {
+                movieRepository.deleteMovieCollect(data.asMovieCardResult())
+            } else {
+                movieRepository.insertMovieCollect(data.asMovieCardResult())
+            }
+        }
+    }
 }
