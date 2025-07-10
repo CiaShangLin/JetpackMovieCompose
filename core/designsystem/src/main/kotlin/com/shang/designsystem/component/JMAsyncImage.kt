4fe0030f0ff773c12a60b3ab261c6efac30e5145
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-10 14:48:09 +0800

    refactor(image): 優化 JMAsyncImage 函式以簡化圖片請求邏輯

--- core/designsystem/src/main/kotlin/com/shang/designsystem/component/JMAsyncImage.kt
+++ core/designsystem/src/main/kotlin/com/shang/designsystem/component/JMAsyncImage.kt
@@ -8,11 +8,9 @@ import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import coil3.compose.AsyncImage
 import coil3.compose.AsyncImagePainter.Companion.DefaultTransform
 import coil3.compose.AsyncImagePainter.State
-import coil3.request.ImageRequest
 
 @Composable
 fun JMAsyncImage(
@@ -29,7 +27,7 @@ fun JMAsyncImage(
     clipToBounds: Boolean = true,
 ) {
     AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current).data(model).build(),
+        model = model,
         contentDescription = contentDescription,
         modifier = modifier,
         transform = transform,
