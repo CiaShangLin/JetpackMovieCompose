7486622ce0f5eb0439fdd6de59cff043d2d5a92c
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-11 14:30:32 +0800

    feat(ui): 更新搜尋畫面，新增隱藏鍵盤功能

--- feature/search/src/main/java/com/shang/search/SearchScreen.kt
+++ feature/search/src/main/java/com/shang/search/SearchScreen.kt
@@ -1,6 +1,5 @@
 package com.shang.search
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -26,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
@@ -45,6 +45,7 @@ fun SearchScreen(viewModel: SearchViewModel = hiltViewModel()) {
     var inputText by remember { mutableStateOf("") }
     val movieSearchPager = viewModel.movieSearchPager.collectAsLazyPagingItems()
     val query by viewModel.searchQueryFlow.collectAsState()
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(modifier = Modifier.fillMaxSize()) {
         TextField(
@@ -58,7 +59,7 @@ fun SearchScreen(viewModel: SearchViewModel = hiltViewModel()) {
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
             keyboardActions = KeyboardActions(
                 onSearch = {
-                    Log.d("DEBUG", "onSearch: $inputText")
+                    keyboardController?.hide()
                     viewModel.startSearch(inputText)
                 },
             ),
