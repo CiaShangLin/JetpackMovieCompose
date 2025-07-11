cd1570a0a3623575259917b5e8f4b7ea3152d388
CaiShangLin <nick034po@yahoo.com.tw>
2025-07-11 14:19:47 +0800

    feat(ui): 新增載入畫面組件與 Lottie 動畫配置

---
+++ core/ui/src/main/kotlin/com/shang/ui/LoadingScreen.kt
@@ -0,0 +1,24 @@
+package com.shang.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever,
+    )
+    LottieAnimation(
+        modifier = modifier,
+        composition = composition,
+        progress = { progress },
+    )
+}
